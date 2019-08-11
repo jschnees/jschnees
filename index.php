436b70faef2feb4063cb4f95e379633351d0b85c
Justin Schnees <jschnees@users.noreply.github.com>
2019-08-10 20:45:24 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,93 @@
+<!--Crafted by Justin Schnees jschnees.com-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- meta tags -->
+	<meta charset="UTF-8">
+	<meta name="description" content="Justin Schnees Personal Portfolio">
+	<meta name="author" content="Justin Schnees">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- links to assets -->
+  <link rel="stylesheet" href="assets/css/style.css" type="text/css" media="all" />
+  <link rel="stylesheet" href="assets/css/nav.css" type="text/css" media="all" />
+  <script defer src="https://use.fontawesome.com/releases/v5.0.4/js/all.js"></script>
+  <script type="text/javascript" src="assets/js/menu.js" async></script>
+  <title>Justin Schnees Design</title>
+</head>
+<body>
+<header>
+  <!-- Naviation -->
+  <div class="nav">
+      <div class="nav-header">
+      </div>
+      <div class="nav-btn">
+        <label for="nav-check">
+          <span></span>
+          <span></span>
+          <span></span>
+        </label>
+      </div>
+      <input type="checkbox" id="nav-check">
+      <div class="nav-links">
+       <a href="index.html" class="nav-links" title="Work">Work</a>
+       <a href="about.html" class="nav-links" title="About">About</a>
+       <a href="Justin_Schnees_Resume.pdf" class="nav-links" title="CV" target="_blank">CV</a>
+      </div>
+    </div>
+</header>
+<Main>
+
+  <div class="Work">
+    
+    <div>
+    <!-- Project 1 -->
+      
+    <div class="MainItem"></div>
+    <div class="MainItem"><a href="businesscards.html" title="Freelance Work"><img alt="Business Cards" class="MainItemIMG" src="assets/img/businesscards.png" alt="Business Cards" />
+    <div class="overlay">
+            <h1>InterFax & EatNGage</h1>
+        </div>
+        </a>
+      </div>
+    </div>
+
+    <div>
+      <!-- Project 2 -->
+      
+        <div class="MainItem"></div>
+        <div class="MainItem"><a href="pcewebsite.html" title="PC Engineers"><img alt="PCE Website" class="MainItemIMG" src="assets/img/pcesite.png" alt="PC Engineers" />
+        <div class="overlay">
+            <h1>PC Engineers</h1>
+        </div>
+        </a>
+      </div>
+    </div>
+
+    <div>
+      <!-- Project 3 -->
+      
+    <div class="MainItem"></div>
+        <div class="MainItem"><a href="projectrb.html" title="Project Red Bead"><img alt="" class="MainItemIMG" src="assets/img/projectrb.png" alt="Project Red Bead" />
+        <div class="overlay">
+	      <h1>Project Red Bead</h1>
+        </div>
+        </a>
+        </div>
+    </div>
+
+    <div>
+      <!-- Project 4 -->
+      
+    <div class="MainItem"></div>
+    <div class="MainItem"><a href="genjiGame.html" title="Genji Game"><img alt="" class="MainItemIMG" src="assets/img/genjiGame.png" alt="Genji Game" />
+        <div class="overlay">
+      <h1>Genji Game</h1>
+        </div>    
+        </a>
+      </div>
+    </div>
+	  
+    </div>
+  </Main>
+  </body>  
+  </html>
